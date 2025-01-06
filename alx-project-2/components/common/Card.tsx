e7e72fb78f3b4be808276b8a0d73e6572ca8b073
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-06 11:48:10 +0200

    PostModal

--- alx-project-2/components/common/Card.tsx
+++ alx-project-2/components/common/Card.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="w-fit flex flex-col justify-start gap-3 p-4 border-gray-400 border shadow-md rounded-md cursor-pointer">
-      <h1 className="font-lg font-semibold">{title}</h1>
-      <p className="font-medium">{content}</p>
+    <div className=" flex flex-col justify-start gap-3 p-4 border-gray-400 border shadow-md rounded-md cursor-pointer">
+      <h1 className="font-lg font-semibold bg-amber-100 p-3 rounded-md shadow-md">
+        {title}
+      </h1>
+      <p className="font-medium p-3">{content}</p>
     </div>
   );
 };
--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,10 +1,30 @@
 import Card from "@/components/common/Card";
 import { POSTS } from "@/constants";
+import PostModal from "@/components/common/PostModal";
+import { useState } from "react";
+
 export default function Home() {
+  const [posts, setPosts] = useState(POSTS);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddPost = (post) => {
+    setPosts((prevPosts) => [...prevPosts, post]);
+  };
   return (
     <div className="w-[85%] mx-auto">
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="px-4 py-2 bg-green-500 text-white rounded"
+      >
+        Add New Post
+      </button>
+      <PostModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSave={handleAddPost}
+      />
       <section className="grid grid-cols-3 gap-4">
-        {POSTS.map((post, i) => (
+        {posts.map((post, i) => (
           <Card title={post.title} content={post.content} key={i} />
         ))}
       </section>
