d4aadc788fcb7043bc4004dfd5deeb885627a40f
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-06 10:18:13 +0200

    routing and nav

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <ul className="flex justify-around items-center p-4 no-underline ">
+        <li className="hover:text-gray-300">
+          <Link href="/home">Home</Link>
+        </li>
+        <li className="hover:text-gray-300">
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Header;
