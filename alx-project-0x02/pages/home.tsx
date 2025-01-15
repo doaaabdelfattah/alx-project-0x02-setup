7aea0eaa0f388f8aa6a79d0d263adb2bd1ce0234
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-16 00:34:18 +0200

    check4

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Card from "@/components/common/Card";
+import { CardProps } from "@/interfaces";
 const home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Card title="hello" content="bodyContent" />
+      <Card title="title 2" content="bodyContent 2" />
+    </div>
+  );
 };
 
 export default home;
