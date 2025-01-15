1812784f3df34ef6081ad6b6d4927a57e2dfab62
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-16 00:48:30 +0200

    task7

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -3,28 +3,37 @@ import PostCard from "@/components/common/PostCard";
 import UserCard from "@/components/common/UserCard";
 import React from "react";
 import { useState, useEffect } from "react";
-const Users = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      users: data, // Pass the fetched data as props
+    },
+  };
+}
 
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+const Users = ({
+  users,
+}: {
+  users: Array<{
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    address: object;
+  }>;
+}) => {
+  if (!users || users.length === 0) return <p>No user data available.</p>;
   return (
     <>
       <h1 className="text-5xl font-semibold text-center my-10 text-orange-800">
         All Users
       </h1>
       <div className=" w-[85%] grid grid-cols-2 gap-10 mx-auto">
-        {data.map((user) => (
+        {users.map((user) => (
           <UserCard
             name={user.name}
             username={user.username}
